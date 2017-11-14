7e62561e6f2ca89c92181fe1cbc0d3ffd91f57a7
irem atlas <irem.atlass@gmail.com>
2017-11-14 22:25:39 +0300

    commit3

--- off day off time/off day off time/hobiler.Designer.vb
+++ off day off time/off day off time/hobiler.Designer.vb
@@ -22,8 +22,101 @@ Partial Class hobiler
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.kaydetb = New System.Windows.Forms.Button()
+        Me.devamb = New System.Windows.Forms.Button()
+        Me.hobib = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"SANAT", "SAĞLIK", "BİLİM", "YAŞAM"})
+        Me.ComboBox1.Location = New System.Drawing.Point(77, 54)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(77, 98)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Kategori:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "İlgi Alanı:"
+        '
+        'kaydetb
+        '
+        Me.kaydetb.Location = New System.Drawing.Point(306, 280)
+        Me.kaydetb.Name = "kaydetb"
+        Me.kaydetb.Size = New System.Drawing.Size(75, 23)
+        Me.kaydetb.TabIndex = 4
+        Me.kaydetb.Text = "Kaydet"
+        Me.kaydetb.UseVisualStyleBackColor = True
+        '
+        'devamb
+        '
+        Me.devamb.Location = New System.Drawing.Point(420, 280)
+        Me.devamb.Name = "devamb"
+        Me.devamb.Size = New System.Drawing.Size(75, 23)
+        Me.devamb.TabIndex = 5
+        Me.devamb.Text = "Devam Et"
+        Me.devamb.UseVisualStyleBackColor = True
+        '
+        'hobib
+        '
+        Me.hobib.Location = New System.Drawing.Point(42, 280)
+        Me.hobib.Name = "hobib"
+        Me.hobib.Size = New System.Drawing.Size(110, 23)
+        Me.hobib.TabIndex = 6
+        Me.hobib.Text = "Yeni Hobi Ekle"
+        Me.hobib.UseVisualStyleBackColor = True
+        '
+        'hobiler
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 335)
+        Me.Controls.Add(Me.hobib)
+        Me.Controls.Add(Me.devamb)
+        Me.Controls.Add(Me.kaydetb)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "hobiler"
         Me.Text = "hobiler"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents kaydetb As Button
+    Friend WithEvents devamb As Button
+    Friend WithEvents hobib As Button
 End Class
--- off day off time/off day off time/hobiler.vb
+++ off day off time/off day off time/hobiler.vb
@@ -1,3 +1,158 @@
 ﻿Public Class hobiler
+    Dim i = 0, k = 0, j As Integer = 0
+    Dim left = 0, top As Integer = 0
 
+    Private Sub textboxolustur()
+
+        i = i + 1
+        Dim textbox As New TextBox()
+
+        textbox.Top = 20 + top
+
+        textbox.Left = 255
+
+        textbox.Width = 100
+
+        textbox.Text = ""
+        textbox.Name = "textbox" & i
+        Me.Controls.Add(textbox)
+        top = top + 25
+
+
+
+
+    End Sub
+    Private Sub labelolustur(ByVal soru As String)
+
+        j = j + 1
+        Dim label As New Label()
+
+        label.Top = 20 + top
+
+        label.Left = 250
+
+        label.Width = 150
+
+
+        label.Text = soru
+        label.Name = "label" & j
+        Me.Controls.Add(label)
+        top = top + 30
+
+
+
+
+    End Sub
+    Private Sub checkboxolustur(ByVal soru As String)
+
+        k = k + 1
+        Dim check As New CheckBox()
+
+        check.Top = 20 + top
+
+        check.Left = 250
+
+
+
+        check.Text = soru
+        check.Name = "check" & k
+        Me.Controls.Add(check)
+        top = top + 25
+
+
+
+    End Sub
+
+    Private Sub ComboBox1_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles ComboBox1.SelectionChangeCommitted
+        Select Case ComboBox1.SelectedItem
+            Case "SANAT"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Müzik")
+                ComboBox2.Items.Add("Edebiyat")
+                ComboBox2.Items.Add("Sinema")
+                ComboBox2.Items.Add("Tiyatro")
+            Case "SAĞLIK"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Spor")
+                ComboBox2.Items.Add("Beslenme")
+            Case "BİLİM"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Teknoloji")
+                ComboBox2.Items.Add("Pozitif Bilimler")
+            Case "YAŞAM"
+                ComboBox2.Items.Clear()
+                ComboBox2.Items.Add("Gezi")
+                ComboBox2.Items.Add("Doğa")
+
+        End Select
+
+    End Sub
+
+    Private Sub ComboBox2_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles ComboBox2.SelectionChangeCommitted
+
+        Select Case ComboBox2.SelectedItem
+            Case "Müzik"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Edebiyat"
+                labelolustur("Kitap okur musunuz?")
+                checkboxolustur("Evet")
+                checkboxolustur("Hayır")
+
+                labelolustur("Ne tür kitap okursunuz?")
+                    textboxolustur()
+
+            Case "Sinema"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Tiyatro"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Spor"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Beslenme"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Teknoloji"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Pozitif Bilimler"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Gezi"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+            Case "Doğa"
+                labelolustur("Sevdiğiniz müzik türü nedir?")
+                textboxolustur()
+                labelolustur("Ne tür müzik dinlersiniz?")
+                textboxolustur()
+        End Select
+
+
+
+    End Sub
+
+    Private Sub hobiler_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
 End Class
